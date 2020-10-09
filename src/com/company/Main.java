a47aed5db7d13f3010a678a1f77bc3c5454d68a4
James Kenney <jkenney2@charter.net>
2020-10-09 17:40:14 -0400

    changed wording again

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Goodbye, Herb and Squiggy");
+        System.out.println("Goodbye, Peaches and Herb and Squiggy");
     }
 
 }
